5130537ba07a7863cbe6f877947cfd9c965685a5
Sorokovsky <Sorokovskys@ukr.net>
2025-03-15 18:54:23 +0200

    refactor

--- wp-content/themes/landshaft/functions.php
+++ wp-content/themes/landshaft/functions.php
@@ -3,7 +3,7 @@ add_action('wp_enqueue_scripts', function(){
     wp_deregister_script( 'jquery' );
     wp_deregister_script( 'jquery-core' );
     wp_register_style('fonts', get_stylesheet_directory_uri() . '/assets/css/swiper.css');
-    wp_register_style('fonts-2', 'https://fonts.gstatic.com', array(), false);
+    wp_register_style('fonts-2', 'https://fonts.gstatic.com');
     wp_register_style('swiper', get_stylesheet_directory_uri() . '/assets/css/swiper.css');
     wp_enqueue_style('style', get_stylesheet_directory_uri() . '/assets/css/style.css', array('fonts', 'fonts-2', 'swiper'), 'null', false);
     wp_register_script('jquery', 'https://code.jquery.com/jquery-3.5.1.min.js', array(), true);
