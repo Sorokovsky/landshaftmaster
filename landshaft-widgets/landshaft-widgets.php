84d033d07012c99f5c44bcbeb790d0d01f238000
Sorokovsky <Sorokovskys@ukr.net>
2025-07-08 19:46:42 +0300

    start widgets plugin

---
+++ landshaft-widgets/landshaft-widgets.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Landshaft widgets
+ * Author:          Sorokovsky Andrey
+ * Text Domain:     landshaft-widgets
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         LandshaftWidgets
+ */
+
+if(!defined('ABSPATH')) {
+    exit();
+}
